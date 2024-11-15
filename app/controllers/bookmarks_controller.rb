dd6c855ede217da6868442afb287063b311a3114
Belen Chiarino <beluchiarino@hotmail.com>
2024-11-15 13:06:48 +0100

    update

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -10,13 +10,16 @@ def create
   @bookmark = Bookmark.new(bookmark_params)
   @bookmark.list = @list
   if @bookmark.save
-    redirect_to lists_path(@list), notice: "Movie List was successfully created."
+    redirect_to list_path(@list), notice: "Movie List was successfully created."
   else
     render :new, status: :unprocessable_entity
   end
 end
 
-def delete
+def destroy
+  @bookmark = Bookmark.find(params[:id])
+  @bookmark.destroy
+  redirect_to list_path(@bookmark.list)
 end
 
 private
